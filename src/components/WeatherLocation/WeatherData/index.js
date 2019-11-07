4f0a634469c78b5d8c83ebaa5128dfcf387ed191
Brenda Martinez <bmartinez@aconcaguasf.com>
2019-11-07 15:05:02 -0300

    VIDEO 5 TERMINADO

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherTemperature from './WeatherTemperature';
 import WeatherExtraInfo from './WeatherExtraInfo';
-import { SNOW } from '../../../constants/weathers';
+
 import './styles.css';
 
-const WeatherData = () => (
+//le paso por parametro la data que dsps voy a usar
+const WeatherData = ({ data }) => {
+    const { temperature, weatherState, humidity, wind } = data;
+    return (
     <div className='weatherDataCont'> 
     {/* asi invocamos las clases del css */}
-        <WeatherTemperature temperature={20} weatherState={SNOW}/>
-        <WeatherExtraInfo humidity={80} wind={'10m/s'}/>
-    </div>
-);
+        <WeatherTemperature temperature={temperature} weatherState={weatherState}/>
+        <WeatherExtraInfo humidity={humidity} wind={wind}/>
+    </div>);
+};
+
+WeatherData.propTypes = {
+    data: PropTypes.shape({
+        //espero un obj de esta forma
+        temperature: PropTypes.number.isRequired,
+        weatherState: PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind: PropTypes.string.isRequired,
+
+    }),
+}
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import WeatherTemperature from './WeatherData/WeatherTemperature';
+import { SUN, CLOUD } from '../../constants/weathers';
 import './styles.css';
 
-const WeatherLocation = () => (
+const data1= {
+    temperature: 20,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '10 m/s',
+};
+
+const data2= {
+    temperature: 10,
+    weatherState: CLOUD,
+    humidity: 0,
+    wind: '20 m/s',
+};
+
+//functional component
+/* const WeatherLocation = () => (
     <div className="weatherLocationCont">
         <Location city = {'Mendoza!'}/>
-        <WeatherData/>
+        <WeatherData data={data}/>
     </div>
-);
+); */
 
-export default WeatherLocation;
+//class component tiene otros atributos para manejar
+class WeatherLocation extends Component {
 
+    //cuando se construye este componente voy a usar este state (el functional compo no lo tiene)
+    constructor() {
+        super();
+        this.state = {
+            city: 'Mendoza',
+            data: data1
+            }; 
+    }
+    
+    handleUpdateClick = () => {
+        this.setState ({
+            city: 'Mendoza',
+            data: data2
+        });
+        console.log("actualizado");
+    }
+
+    render = () => {
+        const { city, data } = this.state;
+        return ( 
+            <div className="weatherLocationCont">
+                <Location city = {city} />
+                <WeatherData data= {data} />
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>);
+    };
+};  
+    export default WeatherLocation;
